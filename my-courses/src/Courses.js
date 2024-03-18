146ef34f07039b2f2e347efb3b0275a90eb168d3
Anar-Mammadzada <anarmammadza@gmail.com>
2024-03-18 11:38:41 +0400

    install react-icons and make a carousel one-by-one

--- my-courses/src/Courses.js
+++ my-courses/src/Courses.js
@@ -1,19 +1,70 @@
-import Course from "./Course";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
-function Courses({courses, removeCourse}){
-    return(
+function Courses({ courses }) {
+
+    const [index, setIndex] = useState(0);
+    const { content, title, price } = courses[index];
+
+    const checkIndex = (index) =>{
+        if(index < 0){
+            return courses.length - 1;
+        }
+
+        if(index > courses.length - 1){
+            return 0;
+        }
+        return index;
+    }
+
+    const prevCourse = () =>{
+        setIndex((index) =>{
+            let newIndex = index - 1;
+            return checkIndex(newIndex);
+        })
+    }
+
+    const nextCourse = () =>{
+        setIndex((index) =>{
+            let newIndex = index + 1;
+            return checkIndex(newIndex);
+        })
+    }
+
+    const getRandomCourse = () =>{
+      let randomNumber =  Math.floor(Math.random()*courses.length)
+
+      if(randomNumber === index){
+        randomNumber = index + 1;
+      }
+        setIndex(checkIndex(randomNumber));
+    }
+
+    
+
+
+    return (
         <div className="courseMainDiv">
-            <div>
+            <div className="courseTitleAndButton">
                 <h2>My Courses</h2>
+                <button className="cardDelete" onClick={getRandomCourse}>
+                    Get random course !
+                </button>
             </div>
             <div className="cardDiv">
-               {
-                courses.map((course) =>{
-                    return (
-                        <Course key={course.id} {...course} removeOneCourse={removeCourse} />
-                    )
-                })
-               }
+                <button className="prevNextBtn" onClick={prevCourse}>
+                    <FaChevronLeft />
+                </button>
+                <div className="card">
+                    <div className="cardTitlePrice">
+                        <h2 className="cardTitle">{title}</h2>
+                        <h4 className="cardPrice">{price}$</h4>
+                    </div>
+                    <p>{content}</p>
+                </div>
+                <button className="prevNextBtn" onClick={nextCourse}>
+                    <FaChevronRight />
+                </button>
             </div>
         </div>
     )
